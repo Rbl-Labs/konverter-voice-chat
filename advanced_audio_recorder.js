410b532e150f8cd946f55d41fbcaf44a588601df
happymik <mikhaill@mironov.io>
2025-06-05 22:39:52 +0200

    Deploy frontend updates

--- advanced_audio_recorder.js
+++ advanced_audio_recorder.js
@@ -1,6 +1,7 @@
 /**
  * AdvancedAudioRecorder manages microphone input using Web Audio API and an AudioWorklet.
  * It provides a continuous stream of PCM audio chunks.
+ * Inspired by: https://github.com/ViaAnthroposBenevolentia/gemini-2-live-api-demo
  */
 export class AdvancedAudioRecorder {
     constructor(options = {}) {
